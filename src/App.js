f7ab6bee28f03456439ecaec8cf99e86c6d2bdf7
VladZimin <vlad.zimin1@gmail.com>
2022-05-12 14:21:15 +0500

    add components (Header, Categories, PizzaCard, Sort)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css'
+import './scss/app.scss'
+import Header from './components/Header'
+import Categories from './components/Categories'
+import Sort from './components/Sort'
+import PizzaCard from './components/PizzaCard'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Salam!</h1>
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            <PizzaCard />
+            <PizzaCard />
+            <PizzaCard />
+            <PizzaCard />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
